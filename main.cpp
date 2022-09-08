9eaf752d8b10a68df91246c78fabd3d48d08e870
eeP3ti <112974335+eeP3ti@users.noreply.github.com>
2022-09-08 18:59:43 +0200

    Hibák javítása

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;//nem kell compile alatt ezt csinálni
+const int N_ELEMENTS = 100; //nem kell compile alatt ezt csinálni
 
 int main()
 {
-    int *b = new int[NELEMENTS];// el van irva a név
-    std::cout << '1-100 ertekek duplazasa' // nincs ';' a végén és végén legyen uj sor; 
-    for (int i = 0;) // hiányoznak for ciklus paraméterei
-    {
-        b[i] = i * 2;// 0-99 helyett 1- 100 ig számoljon
-    }
-    for (int i = 0; i; i++) // feltétel miatt nem fog belekerülni a ciklusba egyszer se
-    {
-        std::cout << "Ertek:"// nem ír ki semit értéknek
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;// alap érték hozzáadása
-    for (int i = 0; i < N_ELEMENTS, i++)// for ciklus paramétereit ;-el illik elválasztani
-    {
-        atlag += b[i]// ; hiányzik a sor végéről
-    }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+	int *b = new int[N_ELEMENTS]; // el van irva a név
+	std::cout << "1-100 ertekek duplazasa\n"; // nincs ';' a végén és végén legyen uj sor; 
+	for (int i = 0; i < N_ELEMENTS;i++) // hiányoznak for ciklus paraméterei
+		{
+			b[i] = (i+1) * 2; // 0-99 helyett 1- 100 ig számoljon
+		}
+	for (int i = 0; i<N_ELEMENTS; i++) // feltétel miatt nem fog belekerülni a ciklusba egyszer se
+	{
+		std::cout << "Ertek:"<< b[i]<< "\n"; // nem ír ki semit értéknek
+	}
+	std::cout << "Atlag szamitasa: " << std::endl;
+	double atlag=0; // alap érték hozzáadása
+	for (int i = 0; i < N_ELEMENTS; i++) // for ciklus paramétereit ;-el illik elválasztani
+	{
+		atlag += b[i]; // ; hiányzik a sor végéről
+	}
+	atlag /= N_ELEMENTS;
+	std::cout << "Atlag: " << atlag << std::endl;
+	system("pause");
+	return 0;
 }
